a55674087199565685af075fe3f933337782f3e8
devansh332 <devanshsaini022@gmail.com>
2021-07-04 20:51:50 +0530

    todo app css

--- todo/src/component/todoItem.jsx
+++ todo/src/component/todoItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {AiOutlineDelete} from "react-icons/ai";
 
 const TodoItem = ({id,content,isChecked,onChange,deletetask}) => {
     return (
@@ -6,7 +7,7 @@ const TodoItem = ({id,content,isChecked,onChange,deletetask}) => {
         <div className="todoitems">
             <input type= "checkbox" defaultChecked = {isChecked} id={id} onChange={onChange}/> 
             <label htmlFor={id}>{content}</label>
-            <button className="btn" onClick ={()=>deletetask(id)} >Delete</button>
+            <button class="btn" onClick ={()=>deletetask(id)}><AiOutlineDelete/></button>
         </div>
         </React.Fragment>
     )
